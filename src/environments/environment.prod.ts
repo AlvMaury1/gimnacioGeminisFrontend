7f7bfc5284ff9a5c48eaa501655cee93c8f3432e
AlvMauri <maurirada987@gmail.com>
2026-06-07 17:15:03 -0400

    v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend.railway.app',
+  apiUrl: 'https://gimnacio-geminis-1.onrender.com',
   recaptchaSiteKey: '6Lfbi9csAAAAAF_1dsw73PcvOmb9wcg-zJQX4LT9'
 };
